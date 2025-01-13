a06a3fdd4c694356f17aebb5e842779b873a8e8c
VinodAkula <vinod.akula@immersivelabz.com>
2025-01-13 10:08:07 +0530

    Minimizing batch window

--- SetupCreator/Files/OpenCadavizDicomViewer.bat
+++ SetupCreator/Files/OpenCadavizDicomViewer.bat
@@ -1,5 +1,13 @@
 @echo off
 
+:: Check if the script is already running in the background
+if "%1"=="background" goto :main
+
+:: Relaunch the script in the background with elevated privileges
+start /min "" "%~f0" background
+exit /B
+
+:main
 set folderPath="C:\Program Files (x86)\CadavizDicomViewer"
 
 echo **Preparing CADAVIZ DICOM VIEWER**
@@ -10,16 +18,17 @@ if %errorlevel% neq 0 (
     exit /B 1
 )
 
+:: Ensure Docker is running with elevated privileges and minimized to system tray
 echo Checking if Docker is running...
 docker info >nul 2>&1
 if %errorlevel% neq 0 (
     echo Docker is not running. Starting Docker and accepting license...
-    start "" "C:\Program Files\Docker\Docker\Docker Desktop.exe" -AcceptLicense
+    start /min "" "C:\Program Files\Docker\Docker\Docker Desktop.exe" -AcceptLicense -Hidesingleton 
     echo Waiting for Docker to start...
     timeout /t 10 /nobreak >nul
 )
 
-rem Wait for Docker to be fully operational
+:: Wait for Docker to be fully operational
 set maxRetries=5
 set /a retries=0
 
@@ -40,6 +49,17 @@ if %errorlevel% neq 0 (
 
 echo Docker Subscription Service Agreement accepted.
 
+:: Granting necessary permissions using icacls
+echo Granting necessary permissions to the folder using icacls...
+powershell -Command "icacls 'C:\Program Files (x86)\CadavizDicomViewer\volumes' /grant Everyone:F /T"
+
+if %errorlevel% neq 0 (
+    echo Failed to grant permissions. Check if the script is running with elevated privileges.
+    pause
+    exit /B 1
+)
+
+:: Adding images
 echo Adding image "ohif_viewer"...
 docker load -i ohif_viewer.tar
 if %errorlevel% neq 0 (
@@ -52,11 +72,11 @@ echo Adding image "pacs"...
 docker load -i pacs.tar
 if %errorlevel% neq 0 (
     echo Failed to load "pacs.tar". Make sure the file exists in the folder.
-     pause
+    pause
     exit /B 1
 )
 
-
+:: Running Docker Compose
 echo Running Docker...
 docker-compose up
 if %errorlevel% neq 0 (
@@ -65,7 +85,7 @@ if %errorlevel% neq 0 (
     exit /B 1
 )
 
-rem If Docker Compose runs successfully, set registry key
+:: Set registry key for Cadaviz
 echo Setting registry key for Cadaviz...
 reg add "HKEY_CURRENT_USER\Software\ImmersiveLabz\Cadaviz" /v IsDockerReady /t REG_SZ /d "true" /f
 if %errorlevel% neq 0 (
@@ -75,5 +95,4 @@ if %errorlevel% neq 0 (
 )
 
 echo Operation completed successfully.
-pause
 exit /B
